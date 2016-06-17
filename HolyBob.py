95a543bb8107410906429820edb11987a488d932
AndriyLazorenko <andriy.lazorenko@gmail.com>
2016-06-17 09:11:55 +0300

    Russian changed to eng. Non-ASCII character '\xd0' in file HolyBob.py on line 23, but no encoding declared; see http://python.org/dev/peps/pep-0263/ for details 2016-06-16T23:35:29.953544+00:00 app[web.1]:   File "HolyBob.py", line 23 2016-06-16T23:35:30.601401+00:00 heroku[web.1]: Process exited with status 1
    Procfile changed to include python HolyBob.py and exclude demo bullshit
    added -p $PORT to procfile
    requirements.txt not include gunicorn now
    
    if name = main, port configured, os removed from reqs
    1 adj per name, showing off
    Russian test

--- HolyBob.py
+++ HolyBob.py
@@ -1,10 +1,22 @@
-# -​- coding: utf-8 -​-
+from __future__ import print_function
+
 from flask import Flask, render_template, request, jsonify
 import numpy as np
 import os as os
+from nltk.corpus import wordnet as wn
 
 app = Flask(__name__)
 past_names = dict()
+adjectives =[synset.name().split('.')[0] for synset in list(wn.all_synsets('a'))]
+adjs = {1: "holy",
+        2: "godlike",
+        3: "wise",
+        4: "allowing me to go to interview",
+        5: "stylish",
+        6: "kind",
+        7: "intellectual",
+        8: "amazing",
+        9: "cool"}
 
 
 @app.route('/')
@@ -22,6 +34,9 @@ def get_response():
     # print(open(r'requirements.txt', 'r').read())
     # with open(os.path.abspath("HolyBob/russian.txt"), 'r') as f:
     #     first_line = f.readline()
+    # with open("templates/russian.txt") as f:
+    #     for line in f:
+    #         print(line)
     greeting = "{} {} {}!".format(greet_line, past_names[text], text).encode("utf-8")
     response = greeting.decode("utf-8")
 
@@ -29,19 +44,37 @@ def get_response():
 
 
 def get_adjective():
-    adjs = {1:"holy",
-            2:"godlike",
-            3:"wise",
-            4:"allowing me to go to interview",
-            5:"stylish",
-            6:"kind",
-            7:"intellectual",
-            8:"amazing",
-            9:"cool"}
     randkey = np.random.randint(1, 10)
-    print(randkey)
-    return adjs.get(randkey)
+    forRet = ""
+    for key, value in adjs.items():
+        if key == randkey:
+            forRet = adjs[key]
+            adjs[key]= get_NLTK_adjective()
 
+    print(adjs.items())
+    return forRet
+
+def get_NLTK_adjective():
+    randkey = np.random.randint(len(adjectives))
+    return adjectives.pop(randkey)
+
+def get_translated_adjective():
+    from nltk.corpus import wordnet as wn
+    from yandex_translate import YandexTranslate
+
+    api_key = 'trnsl.1.1.20160617T052514Z.993b5acbcc92e653.80b6a7746c955a46dbf29c6b92f52eebdb657e77'
+    translate = YandexTranslate(api_key)
+
+    adjectives = [synset.name().split('.')[0] for synset in list(wn.all_synsets('a'))]
+    for i in range(10):
+        while True:
+            adj = adjectives[np.random.randint(len(adjectives))]
+            word = translate.translate(adj, u'en-ru')[u'text'][0]
+            if word.endswith(u'ый') or word.endswith(u'ий'):  # or word.endswith(u'ой'):
+                print(adj, 'Translate:', word)
+                break
+            else:
+                continue
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
