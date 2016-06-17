c40e6a3e22cc0af4a06cd8992c476fe166f3b113
AndriyLazorenko <andriy.lazorenko@gmail.com>
2016-06-17 03:16:17 +0300

    Russian changed to eng. Non-ASCII character '\xd0' in file HolyBob.py on line 23, but no encoding declared; see http://python.org/dev/peps/pep-0263/ for details 2016-06-16T23:35:29.953544+00:00 app[web.1]:   File "HolyBob.py", line 23 2016-06-16T23:35:30.601401+00:00 heroku[web.1]: Process exited with status 1
    Procfile changed to include python HolyBob.py and exclude demo bullshit
    added -p $PORT to procfile
    requirements.txt not include gunicorn now
    
    if name = main, port configured

--- HolyBob.py
+++ HolyBob.py
@@ -44,4 +44,6 @@ def get_adjective():
 
 
 if __name__ == '__main__':
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
