19eea4ca3257ee67740fa4116f0f0326f04dda46
ivan shpotenko <ivan.shpotenko@gmail.com>
2016-09-23 13:19:18 +0300

    another fix

--- wsgi/application.py
+++ wsgi/application.py
@@ -2,11 +2,11 @@
 import os
 
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python3.5/site-packages')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
     with open(virtenv) as f:
-        code = compile(f.read(), "somefile.py", 'exec')
+        code = compile(f.read(), virtenv, 'exec')
         exec(code, dict(__file__=virtualenv))
 except IOError:
     pass
